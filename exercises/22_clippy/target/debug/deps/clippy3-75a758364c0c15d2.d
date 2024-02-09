327e9da875d21a5d78a7470b77053d0cbae20988
puppuccino <57177806+puppuccino@users.noreply.github.com>
2024-02-10 05:06:03 +0900

    rustlings answer

---
+++ exercises/22_clippy/target/debug/deps/clippy3-75a758364c0c15d2.d
@@ -0,0 +1,9 @@
+/workspaces/rustlings/exercises/22_clippy/target/debug/deps/libclippy3-75a758364c0c15d2.rmeta: clippy3.rs Cargo.toml
+
+/workspaces/rustlings/exercises/22_clippy/target/debug/deps/clippy3-75a758364c0c15d2.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
